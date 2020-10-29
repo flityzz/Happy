67cfcc8393a4011d4cdfe21857dcf9adefbeb387
Joao vitor lima dos santos <jvlds.joao@gmail.com>
2020-10-29 19:50:56 -0300

    aprimorando a dashboard

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -2,11 +2,22 @@ import React from "react";
 import { useEffect, useState } from "react";
 import jwt from "jsonwebtoken";
 
-import api from '../services/api'
+import api from "../services/api";
+
+interface User {
+  id: number;
+  email: string;
+  orphanages: Array<{
+    id: string;
+    name: string;
+    latitude: number;
+    longitude: number;
+  }>;
+}
 
 export default function Dashboard() {
   const [token, setToken] = useState<any>("");
-  const [user, setUser] = useState();
+  const [user, setUser] = useState<User>();
 
   useEffect(() => {
     const token = sessionStorage.getItem("@session_token");
@@ -14,11 +25,18 @@ export default function Dashboard() {
     if (token) {
       setToken(token);
       const decoded = jwt.verify(token, "secret");
-      
 
-    } 
-
+      api.get(`/users/${decoded.id}`).then((response) => {
+        setUser(response.data);
+      });
+    }
   }, [token]);
 
-  return token ? <h1>logado</h1> : <h1>nao logado</h1>;
+  return token ? (
+    <div className="user-container" style={{ color: "#000" }}>
+      <h1>logado</h1>
+    </div>
+  ) : (
+    <h1>nao logado</h1>
+  );
 }
