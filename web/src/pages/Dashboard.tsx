557aa0148ad58c10877cf9ee6b5d4c7d7291d710
Joao vitor lima dos santos <jvlds.joao@gmail.com>
2020-11-06 17:54:19 -0300

    adicionando chamadas a api

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -4,15 +4,21 @@ import jwt from "jsonwebtoken";
 
 import api from "../services/api";
 import "../styles/pages/dashboard.css";
-import asideLogo from '../images/dashboard-logo.svg';
+import asideLogo from "../images/dashboard-logo.svg";
 
 import { Link } from "react-router-dom";
 
-import { FiMapPin, FiAlertCircle, FiPower } from 'react-icons/fi';
+import {
+  FiMapPin,
+  FiAlertCircle,
+  FiPower,
+  FiEdit3,
+  FiTrash,
+} from "react-icons/fi";
 import { Map, TileLayer, Marker } from "react-leaflet";
 
-import happyMapIcon from '../utils/mapIcon';
- 
+import happyMapIcon from "../utils/mapIcon";
+
 interface User {
   id: number;
   email: string;
@@ -44,28 +50,38 @@ export default function Dashboard() {
   return token ? (
     <div id="user-container">
       <aside>
-
-        <div className="aside-logo"><img src={asideLogo} alt="logo"/></div>
-
-        <div className="action-buttons">
-          <div className="approved-orphanages"><FiMapPin size={30}/></div>
-          <div className="pending-orphanages"><FiAlertCircle size={30}/></div>
+        <div className="aside-logo">
+          <img src={asideLogo} alt="logo" />
         </div>
 
-        <Link to="/" className="logout"><FiPower size={30}/></Link>
+        <div className="action-buttons">
+          <div className="approved-orphanages">
+            <FiMapPin size={30} />
+          </div>
+          <div className="pending-orphanages">
+            <FiAlertCircle size={30} />
+          </div>
+        </div>
 
+        <Link to="/" className="logout">
+          <FiPower size={30} />
+        </Link>
       </aside>
 
       <div className="dashboard">
         <header>
           <h1 className="title">Orfanatos cadastrados</h1>
-          <span className="orphanages-number">2 orfanatos</span>
+          <span className="orphanages-number">
+            {user?.orphanages.length} orfanatos
+          </span>
         </header>
 
         <div className="orphanages-list">
-          <div className="orphanage">
-          <Map
-                center={[-3.7491403, -38.5568483]}
+          {user?.orphanages.map(orphanage => {
+            return (
+              <div className="orphanage">
+              <Map
+                center={[orphanage.latitude, orphanage.longitude]}
                 zoom={16}
                 style={{ width: "100%", height: 227 }}
                 dragging={false}
@@ -80,24 +96,26 @@ export default function Dashboard() {
                 <Marker
                   interactive={false}
                   icon={happyMapIcon}
-                  position={[-3.7491403, -38.5568483]}
+                  position={[orphanage.latitude, orphanage.longitude]}
                 />
               </Map>
-            <div className="actions">
-              <h1>Orf. Esperança</h1>
+              <div className="actions">
+                <h1>{orphanage.name}</h1>
 
-              <div className="buttons">
-                <Link to='/orphanages/create' className="edit-orphanage"></Link>
-                <div className="delete-orphanage"></div>
+                <div className="buttons">
+                  <Link to="/orphanages/create" className="edit-orphanage">
+                    <FiEdit3 size={30} />
+                  </Link>
+                  <div className="delete-orphanage">
+                    <FiTrash size={30} />
+                  </div>
+                </div>
               </div>
-
             </div>
-          </div>
-          
+            )
+          })}
         </div>
-
       </div>
-      
     </div>
   ) : (
     <h1>nao logado</h1>
